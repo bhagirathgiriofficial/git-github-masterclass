19d15f6dfd2e366e5d8a6cc15001ad868ef10c5a
Naveed Shaikh <heyitsnaveed@gmail.com>
2025-12-01 21:30:05 +0530

    Update app.js
    New JS function

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('first')
+// A more concise way to write the same function using an arrow function
+const calculateCircleAreaModern = (radius) => {
+  // You can make this even shorter since it's a single return expression
+  return Math.PI * radius ** 2;
+};
+
+// --- How to use the function ---
+const radiusValueModern = 10;
+const areaResultModern = calculateCircleAreaModern(radiusValueModern);
+
+console.log(`The area using the modern arrow function with a radius of ${radiusValueModern} is: ${areaResultModern}`);
+// Expected output: The area using the modern arrow function with a radius of 10 is: 314.1592653589793
